d5db91a5f72028719449241d6087b13e96b4a8b3
Parth Khandelwal <45523190+Parthkhandelwal0@users.noreply.github.com>
2021-05-11 23:28:02 +0530

    Update HPContract.sol

--- HPContract.sol
+++ HPContract.sol
@@ -2,23 +2,22 @@ pragma solidity >=0.4.22 <0.7.0;
 
 
 contract PatientHospital {
-    mapping (uint256 => Hospitalrecords) public hospitalrecords;
     mapping (address => Patientrecords) public patientrecords;
     mapping(address => uint) balances;
     mapping(address=>uint)public balanceReceived;
 
-     uint256 public HospitalCount = 0;
      uint256 public PatientCount = 0;
 
     
     struct Hospitalrecords{
         uint Hid;
-        address hospital;
+        address hospitaladdress;
         string name;
         string Haddr;
         uint HPhoneNumber;
     } 
     
+     Hospitalrecords  hospital;
      function addHospitalRecord (
         address _hospitalAddress,
         uint256 _Hid,
@@ -27,14 +26,22 @@ contract PatientHospital {
         uint _HphoneNumber)
         public
         {
-        hospitalrecords[HospitalCount].hospital = _hospitalAddress;
-        hospitalrecords[HospitalCount].Hid = _Hid;
-        hospitalrecords[HospitalCount].name = _name;
-        hospitalrecords[HospitalCount].HPhoneNumber = _HphoneNumber;
-        hospitalrecords[HospitalCount].Haddr = _Haddr;
-        HospitalCount += 1;
+           hospital = Hospitalrecords(_Hid,_hospitalAddress,_name,_Haddr,_HphoneNumber);
+        // Hospitalrecords.hospital = _hospitalAddress;
+        // Hospitalrecords.Hid = _Hid;
+        // Hospitalrecords.name = _name;
+        // Hospitalrecords.HPhoneNumber = _HphoneNumber;
+        // Hospitalrecords.Haddr = _Haddr;
+        // HospitalCount += 1;
     }
     
+    function getHospitalRecord() public view returns( address _hospitalAddress,
+        uint256 _Hid,
+        string  _name,
+        string  _Haddr,
+        uint _HphoneNumber){
+            return(hospital.hospitaladdress,hospital.Hid,hospital.name,hospital.Haddr,hospital.HPhoneNumber );
+        }
     struct Patientrecords{
         uint Pid;
         address _patientAddress;
@@ -85,23 +92,27 @@ contract PatientHospital {
          return(_name,_Paddr,_PPhoneNumber,_BillAmount);
         //  _visitReason = patientrecords[_recordID][_patientAddress].visitReason;
       }
+      function getBill(address _patientAddress) public returns(uint256 _billAmount){
+         _billAmount = patientrecords[_patientAddress].BillAmount;
+         return(_billAmount);
+      }
     
     function pay(address _patientAddress) public payable{
-        
-        require(msg.value==patientrecords[_patientAddress].BillAmount);
+        require(msg.value==patientrecords[_patientAddress].BillAmount,"incorrect amount");
         balanceReceived[msg.sender]+=msg.value;
     }
-    // function balanceOf() external view returns(uint){
-    //     return address(this).balance;
-    //}
-    function withdrawMoney(address _to, uint _amount) public{
-        require(_amount<=balanceReceived[msg.sender],"not enough funds.");
-        balanceReceived[msg.sender]-=_amount;
-        _to.transfer(_amount);
-    } 
-    function hello(string _ab) external pure returns(string memory){
-        return _ab;
+    
+    function balanceOf() external view returns(uint){
+        return address(this).balance;
     }
+    // function withdrawMoney(address _to, uint _amount) public{
+    //     require(_amount<=balanceReceived[msg.sender],"not enough funds.");
+    //     balanceReceived[msg.sender]-=_amount;
+    //     _to.transfer(_amount);
+    // } 
+    // function hello(string _ab) external pure returns(string memory){
+    //     return _ab;
+    // }
 }
 
 
@@ -109,24 +120,43 @@ contract PatientHospital {
 contract HospitalInsurance{
     address addressPatientHospital;
     bool public isCorrect;
+    mapping(address=>uint)public balanceReceived;
+    uint256 public billAmount;
+
 
     function setAddress(address _addressPatientHospital) external {
         addressPatientHospital = _addressPatientHospital;
     }
     
-    function getRecord(address _patientAddress) external view  returns (
+    function getRecord(address _patientAddress) external view returns (
         string _name,
         string _Paddr,
         uint _PPhoneNumber,
         uint _BillAmount)
         {
             PatientHospital patienthospital = PatientHospital(addressPatientHospital);
+           
             return patienthospital.getRecord(_patientAddress);
+            
         }
     
     function checkRecords(bool _recordCheck) public returns(string memory) {
         isCorrect = _recordCheck;
         require(isCorrect==true,"match not found");
         return "match found";
-    }     
+    }
+    
+    function getBill(address _patientAddress) public returns(uint256){
+         PatientHospital patienthospital = PatientHospital(addressPatientHospital);
+         billAmount = patienthospital.getBill(_patientAddress);
+    }
+    
+     function pay(address _patientAddress) public payable{
+        require(msg.value==billAmount,"incorrect amount");
+        balanceReceived[_patientAddress]+=msg.value;
+    }
+    
+    function balanceOf() external view returns(uint){
+        return address(this).balance;
+    }
 }
