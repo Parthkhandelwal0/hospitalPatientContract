bab8b0b632f6f9069405794764883089060a1705
Parth Khandelwal <45523190+Parthkhandelwal0@users.noreply.github.com>
2021-05-14 20:32:50 +0530

    Update HPContract.sol

--- HPContract.sol
+++ HPContract.sol
@@ -6,7 +6,6 @@ contract PatientHospital {
     mapping(address=>uint)public balanceReceived;
     uint256 public PatientCount = 0;
 
-    
     struct Hospitalrecords{
         uint Hid;
         address hospitaladdress;
@@ -14,7 +13,6 @@ contract PatientHospital {
         string Hlocation;
         uint HPhoneNumber;
     } 
-    
      struct Patientrecords{
         uint Pid;
         bool isInsurance;
@@ -26,14 +24,12 @@ contract PatientHospital {
         uint BillAmount;
         string visitReason;
     }
-    
     struct Insurance{
         uint256 InsuranceNo;
         uint256 InsuranceCover;
         string CompanyName;
         string ExpiryDate;
     }
-    
      Hospitalrecords hospital;
      function addHospitalRecord (
         address _hospitalAddress,
@@ -45,7 +41,6 @@ contract PatientHospital {
         {
            hospital = Hospitalrecords(_Hid,_hospitalAddress,_name,_Hlocation,_HphoneNumber);
     }
-    
     function getHospitalRecord() public view returns( address _hospitalAddress,
         uint256 _Hid,
         string  _name,
@@ -53,7 +48,6 @@ contract PatientHospital {
         uint _HphoneNumber){
             return(hospital.hospitaladdress,hospital.Hid,hospital.name,hospital.Hlocation,hospital.HPhoneNumber );
         }
-   
     function addPatientRecord (
         uint _Pid,
         bool _isInsurance,
@@ -78,7 +72,6 @@ contract PatientHospital {
         patientrecords[_patientAddress].visitReason = _visitReason;
         PatientCount += 1;
     }
-    
      function addPatientInsuranceRecord (
         address _patientAddress,
         uint256 _insuranceNo,
@@ -93,7 +86,6 @@ contract PatientHospital {
             patientinsurancerecords[_patientAddress].CompanyName = _companyName;
             patientinsurancerecords[_patientAddress].ExpiryDate = _expiryDate;
         }
-    
      function getPatientRecord( address _patientAddress)
       public
       view
@@ -118,35 +110,28 @@ contract PatientHospital {
          _companyName = patientinsurancerecords[_patientAddress].CompanyName;
          _expiryDate = patientinsurancerecords[_patientAddress].ExpiryDate;
          _visitReason = patientrecords[_patientAddress].visitReason;
-         
          return(_name,_Plocation,_PPhoneNumber,_BillAmount,_insuranceNo,_insuranceCover,_companyName,_expiryDate,_visitReason);
       }
-      
       function Bill(address _patientAddress) public view returns(uint256 _billAmount){
          _billAmount = patientrecords[_patientAddress].BillAmount;
          return(_billAmount);
       }
-          
     function pay(address _patientAddress) public payable returns(string){
         require(msg.value==patientrecords[_patientAddress].BillAmount,"incorrect amount");
         balanceReceived[msg.sender] += msg.value;
         return "payment done";
     }
-    
     function balanceOf() external view returns(uint){
         return address(this).balance;
     }
-    
 }
-
-
+    
 contract HospitalInsurance{
     address addressPatientHospital;
     bool public isCorrect;
     mapping(address=>uint)public balanceReceived;
     uint256 public billAmount;
     address public patientAddress;
-
 
     function setAddress(address _addressPatientHospital) external {
         addressPatientHospital = _addressPatientHospital;
@@ -169,24 +154,21 @@ contract HospitalInsurance{
             PatientHospital patienthospital = PatientHospital(addressPatientHospital);
             return patienthospital.getPatientRecord(patientAddress);
         }
-    
     function checkRecords(bool _recordCheck) public returns(string memory) {
         isCorrect = _recordCheck;
         require(isCorrect==true,"match not found");
         return "match found";
     }
-    
     function getBill() public returns(uint256){
          PatientHospital patienthospital = PatientHospital(addressPatientHospital);
          billAmount = patienthospital.Bill(patientAddress);
     }
-    
      function pay() public payable returns(string){
+        require(isCorrect==true,"incorrect information");
         require(msg.value==billAmount,"incorrect amount");
         balanceReceived[patientAddress]+=msg.value;
         return "payment done by the Insurance Company";
     }
-    
     function balanceOf() external view returns(uint){
         return address(this).balance;
     }
